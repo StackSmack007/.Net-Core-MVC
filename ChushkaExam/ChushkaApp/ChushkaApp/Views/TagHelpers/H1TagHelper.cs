1f771cfaa654b0e5c90eba3ac2b080bae40e4ce6
StackSmack007 <n_hristov007@abv.bg>
2019-09-03 18:47:01 +0300

    add

---
+++ ChushkaExam/ChushkaApp/ChushkaApp/Views/TagHelpers/H1TagHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace ChushkaApp.Views.TagHelpers
+{
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    [HtmlTargetElement("h1")]
+    public class H1TagHelper : TagHelper
+    {
+        public string AspColor { get; set; } = string.Empty;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (AspColor != string.Empty)
+            {
+                output.PostContent.AppendHtml("<p>To be Or not to be!</p>");
+                output.Attributes.SetAttribute("style", "color:" + AspColor);
+            }
+        }
+    }
+}
