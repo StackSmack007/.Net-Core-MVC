0ebe11030f5003f7458d93fc585492630209a713
StackSmack007 <n_hristov007@abv.bg>
2019-09-03 18:46:28 +0300

    update

--- Chushka_Exam/ChushkaExam/ChushkaExam.Infrastructure.Models/ModelsAll/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChushkaExam.Infrastructure.Models.ModelsAll
-{
-    using System.ComponentModel.DataAnnotations;
-    public abstract class BaseEntity<T>
-    {
-        [Key]
-        public T Id { get; set; }
-    }
-}
